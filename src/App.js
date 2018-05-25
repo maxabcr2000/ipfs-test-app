f1c4fb4c146e64c08319119843a19a39cfb96f12
Max <maxabcr2000@gmail.com>
2018-05-25 14:44:37 +0800

    Try to add new peer

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       ipfsHash: "",
+      peer: "",
       title: "Welcome to React"
     };
 
@@ -16,11 +17,11 @@ class App extends Component {
   }
 
   init = async () => {
-    const {ipfs} = this.props;
+    const { ipfs } = this.props;
 
     try {
       ipfs.bootstrap.list((err, res) => {
-        if (err){
+        if (err) {
           console.log("error:", err);
           return;
         }
@@ -28,26 +29,32 @@ class App extends Component {
         console.log("bootstrap peers: ", res.Peers);
       });
 
-    } catch (error){
+    } catch (error) {
       console.log("error:", error);
     }
   }
 
-  handleCheckSwarmPeers = async ()=>{
-    const {ipfs} = this.props;
+  handleCheckSwarmPeers = async () => {
+    const { ipfs } = this.props;
 
     const peers = await ipfs.swarm.peers();
     console.log("Swarm peers:", peers);
   }
 
-  handleChange = (event) => {
+  handleChangeHash = (event) => {
     this.setState({
       ipfsHash: event.target.value
     });
   }
 
+  handleChangePeer = (event) => {
+    this.setState({
+      peer: event.target.value
+    });
+  }
+
   handleCatIPFS = async () => {
-    const {ipfs} = this.props;
+    const { ipfs } = this.props;
 
     try {
       const file = await ipfs.files.cat(this.state.ipfsHash);
@@ -58,7 +65,17 @@ class App extends Component {
         title: file.toString('utf8')
       });
 
-    } catch (error){
+    } catch (error) {
+      console.log("error:", error);
+    }
+  }
+
+  handleConnectToPeer = async () => {
+    const { ipfs } = this.props;
+
+    try {
+      await ipfs.swarm.connect(this.state.peer);
+    } catch (error) {
       console.log("error:", error);
     }
   }
@@ -76,10 +93,14 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <button onClick={this.handleCheckSwarmPeers}>Check swarm peers</button>
-        <br/>
-        <input onChange={this.handleChange} value={this.state.ipfsHash}></input>
-        <br/>
+        <br />
+        <input onChange={this.handleChangeHash} value={this.state.ipfsHash}></input>
+        <br />
         <button onClick={this.handleCatIPFS}> Cat Ipfs data</button>
+        <br />
+        <button onClick={this.handleConnectToPeer}>Connect to peer</button>
+        <br />
+        <input onChange={this.handleChangePeer} value={this.state.peer}></input>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -7,22 +7,40 @@ import registerServiceWorker from './registerServiceWorker';
 
 console.log(window);
 
-// const ipfsNode = new window.Ipfs({ 
-//     repo: 'ipfs-' + Math.random(),
-// });
+const ipfsNode = new window.Ipfs({
+    repo: 'ipfs-' + Math.random(),
+    config: {
+        "Addresses": {
+            "Swarm": [
+                "/dns4/star-signal.cloud.ipfs.team/tcp/443/wss/p2p-webrtc-star/ipfs/QmQGbDesZ8ihd1tA9XHkY7gromHywcvSR7jKb4zxjPqSc2"
+            ],
+            "API": "",
+            "Gateway": ""
+        },
+        "Discovery": {
+            "MDNS": {
+                "Enabled": false,
+                "Interval": 10
+            },
+            "webRTCStar": {
+                "Enabled": true
+            }
+        },
+    }
+});
 
-const ipfsNode = window.ipfs;
+//const ipfsNode = window.ipfs;
 
-// ipfsNode.on('ready', () => {
-//     console.log("Your node is now ready to use!");
-//     console.log('Online status: ', ipfsNode.isOnline() ? 'online' : 'offline');
-//     ReactDOM.render(<App ipfs={ipfsNode}/>, document.getElementById('root'));
-// });
+ipfsNode.on('ready', () => {
+    console.log("Your node is now ready to use!");
+    console.log('Online status: ', ipfsNode.isOnline() ? 'online' : 'offline');
+    ReactDOM.render(<App ipfs={ipfsNode} />, document.getElementById('root'));
+});
 
-// ipfsNode.on('error', (error)=>{
-//     console.log(error);
-// });
+ipfsNode.on('error', (error) => {
+    console.log(error);
+});
 
-ReactDOM.render(<App ipfs={ipfsNode}/>, document.getElementById('root'));
+//ReactDOM.render(<App ipfs={ipfsNode}/>, document.getElementById('root'));
 
 registerServiceWorker();
